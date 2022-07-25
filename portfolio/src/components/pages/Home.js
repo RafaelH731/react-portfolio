f43031f6ac9c39e2ef46e5e75dcd4b023bfe4e75
Rafael Herrera <rafaelherrera731@gmail.com>
2022-07-25 14:45:02 -0700

    refactor folder setup

---
+++ portfolio/src/components/pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Rafael Herrera
+      </h1>
+      <p>
+        My name is Rafael Herrera and I am programmer who has recently started a
+        coding bootcamp through the University of Washington. I have recently been working with a
+        non-profit organization that serves lower income
+        families in the Yakima Valley.
+      </p>
+    </div>
+  );
+}
